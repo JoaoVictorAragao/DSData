018a7d13fe3f3b49ac5d4880549113616fd7688a
JoaoVictorAragao <jojoba2018cnx@gmail.com>
2022-09-05 13:02:16 -0300

    UseState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,6 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
-import './style.css'
+import './style.css';
 
 function NotificarionButton() {
     return (
@@ -10,4 +10,4 @@ function NotificarionButton() {
     )
 }
 
-export default NotificarionButton
+export default NotificarionButton;
