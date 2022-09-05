86ec301fbce5cb0e10ed1fc213757c9500b7072e
JoaoVictorAragao <jojoba2018cnx@gmail.com>
2022-09-05 12:45:05 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import NotificarionButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo</h1>
-      <NotificarionButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+
+            <SalesCard/>
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
